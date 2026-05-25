ffe4e17cdc8af62ff75929600d163f5791055e43
Bachs-JSA05 <bachcaoryan1234@gmail.com>
2026-05-25 22:48:03 +0700

    l1

---
+++ B3.py
@@ -0,0 +1,5 @@
+name_patient = input('Họ và tên bệnh nhân');
+id_patient =  input('Mã bệnh án');
+room_patient = input('Khoa/Phòng khám chỉ định');
+
+print(f'bệnh nhân:{name_patient} - Mã ba:{id_patient} - Chuyển tới:{room_patient}');
